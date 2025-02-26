51f543212412bdad31c7bf55a681ad103b9c1f8b
Sewen <596777598@qq.com>
2025-02-26 10:31:44 +0800

    feat:添加 checkbox 组件示例

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
 </script>
 
 <template>
-<Button>点击按钮</Button>
+  <div class="demo-container">
+    <Button>点击按钮</Button>
+    <Checkbox></Checkbox>
+  </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.demo-container {
+  width: 200px;
+   display: flex;
+   flex-direction: column;
+   gap: 10px;
 }
 </style>
